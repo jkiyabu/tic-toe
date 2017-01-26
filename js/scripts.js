92bab15fa1fe79d0f623933eedb88ba83b1a0d3e
Jayeson Kiyabu and Stella Huayhuaca <stella@>
2017-01-25 16:43:02 -0800

    add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-
+function startGame(){
+  document.getElementById("message").innerHTML = "Let's do this!";
+  document.turn= "X";
+}
 function switchTurn() {
   if (document.turn === "X") {
     document.turn = "O";
@@ -22,23 +25,20 @@ function nextmove(square) {
 
 function getBox(number) {
   return document.getElementById("space"+number).innerHTML;
-
 }
 
 function winner() {
   if(((getBox(1) === "X") && (getBox(2) === "X") && (getBox(3) === "X")) || ((getBox(4) === "X") && (getBox(5) === "X") && (getBox(6) === "X")) || ((getBox(7) === "X") && (getBox(8) === "X") && (getBox(9) === "X")) || ((getBox(1) === "X") && (getBox(5) === "X") && (getBox(9) === "X")) || ((getBox(3) === "X") && (getBox(5) === "X") && (getBox(7) === "X")) || ((getBox(1) === "X") && (getBox(4) === "X") && (getBox(7) === "X")) || ((getBox(2) === "X") && (getBox(5) === "X") && (getBox(8) === "X")) || ((getBox(3) === "X") && (getBox(6) === "X") && (getBox(9) === "X"))){
-    document.getElementById("message").innerHTML = "X is the winner!";
+    document.getElementById("message").innerHTML = "X is the WINNER!!";
     document.turn= "";
 
   }else if(((getBox(1) === "O") && (getBox(2) === "O") && (getBox(3) === "O")) || ((getBox(4) === "O") && (getBox(5) === "O") && (getBox(6) === "O")) || ((getBox(7) === "O") && (getBox(8) === "O") && (getBox(9) === "O")) || ((getBox(1) === "O") && (getBox(5) === "O") && (getBox(9) === "O")) || ((getBox(3) === "O") && (getBox(5) === "O") && (getBox(7) === "O")) || ((getBox(1) === "O") && (getBox(4) === "O") && (getBox(7) === "O")) || ((getBox(2) === "O") && (getBox(5) === "O") && (getBox(8) === "O")) || ((getBox(3) === "O") && (getBox(6) === "O") && (getBox(9) === "O"))){
-    document.getElementById("message").innerHTML = "O is the winner!";
+    document.getElementById("message").innerHTML = "O is the WINNER!!";
     document.turn= "";
   }else if (((getBox(1) != "") && (getBox(2) != "") && (getBox(3) != "") && (getBox(4) != "") && (getBox(5) != "") && (getBox(6) != "") && (getBox(7) != "") && (getBox(8) != "") && (getBox(9) != ""))) {
-    document.getElementById("message").innerHTML = "tie";
+    document.getElementById("message").innerHTML = "Tie, you both LOSE!!";
   }
 }
-
-
 
 function reset() {
   document.getElementById("space1").innerHTML = "";
@@ -50,6 +50,8 @@ function reset() {
   document.getElementById("space7").innerHTML = "";
   document.getElementById("space8").innerHTML = "";
   document.getElementById("space9").innerHTML = "";
+  startGame();
+
 }
 
 //user interface logic:
@@ -57,12 +59,10 @@ function reset() {
 $(document).ready(function() {
   $("button#start").click(function() {
     startGame();
+    $("#board").show();
+    $("button#start").hide();
+    $("button#reset").show();
   })
-
-  function startGame(){
-    document.turn= "X";
-  }
-
   $("button#reset").click(function() {
     reset();
   })
