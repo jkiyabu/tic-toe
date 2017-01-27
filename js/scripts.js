51fb3cf7393e49ce62d9bdde2d85d32e6a46cd35
Jayeson Kiyabu and Stella Huayhuaca <jayeson@>
2017-01-26 16:56:40 -0800

    add winner function using objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,101 @@
+function message(msg){
+  document.getElementById("message").innerHTML = msg;
+}
+
+function startGame(){
+  document.turn= "X";
+  message("Let's do this!") ;
+}
+
+function nextmove(mark) {
+  if(mark.innerText === "") {
+    mark.innerText = document.turn;
+  }else{
+    alert("that space is taken");
+    switchTurn();
+  }
+  switchTurn();
+  return mark.innerText;
+}
+
+function switchTurn() {
+  if (document.turn === "X") {
+    document.turn = "O";
+  } else {
+    document.turn = "X";
+  }
+
+  message(document.turn + " gets to go") ;
+  winner();
+}
+function Square(ynumber, yindex) {
+  this.ynumber = ynumber;
+  this.yindex= yindex;
+}
+var y1 = document.getElementsByClassName("y1");
+var y2 = document.getElementsByClassName("y2");
+var y3 = document.getElementsByClassName("y3");
+
+var newSquare1 = new Square(y1, 0);
+var newSquare2 = new Square(y1, 1);
+var newSquare3 = new Square(y1, 2);
+var newSquare4 = new Square(y2, 0);
+var newSquare5 = new Square(y2, 1);
+var newSquare6 = new Square(y2, 2);
+var newSquare7 = new Square(y3, 0);
+var newSquare8 = new Square(y3, 1);
+var newSquare9 = new Square(y3, 2);
+
+function winner() {
+  if ((newSquare1.ypos() === newSquare2.ypos()) && (newSquare2.ypos() === newSquare3.ypos())){
+
+    message("X is the WINNER!!");
+
+  }else if(((newSquare4.ypos() === newSquare5.ypos()) && (newSquare5.ypos() === newSquare6.ypos()))) {
+    message("O is the WINNER!!");
+
+  }
+    // if (document.turn === "O"){
+    // }else{
+    //   message("O is the WINNER!!");
+    // }
+  document.turn= "";
+  }
+}
+
 
 $(document).ready(function(){
-  function Square(ynumber, yindex) {
-    this.ynumber = ynumber;
-    this.yindex= yindex;
-  }
-
-  Square.prototype.ypos = function () {
-    return this.ynumber[this.yindex].innerText;
-  };
-
-
-  var y1 = document.getElementsByClassName("y1");
-  var y2 = document.getElementsByClassName("y2");
-  var y3 = document.getElementsByClassName("y3");
-
-  var newSquare1 = new Square(y1, 0);
-  var newSquare2 = new Square(y1, 1);
-  var newSquare3 = new Square(y1, 2);
-  var newSquare4 = new Square(y2, 0);
-  var newSquare5 = new Square(y2, 1);
-  var newSquare6 = new Square(y2, 2);
-  var newSquare7 = new Square(y3, 0);
-  var newSquare8 = new Square(y3, 1);
-  var newSquare9 = new Square(y3, 2);
-
-  var squares= [];
-  squares.push(newSquare1,newSquare2,newSquare3,newSquare4,newSquare5,newSquare6,newSquare7,newSquare8,newSquare9);
-
-  for(i = 0; i <= squares.length; i++){
-    return squares[i].ypos();
-  }
-
-  function startGame(){
-    document.getElementById("message").innerHTML = "Let's do this!";
-    document.turn= "X";
-  }
-
-  function nextmove(mark) {
-
-    if(mark.innerText === "") {
-      mark.innerText = document.turn;
-    }else{
-      alert("that space is taken");
-      switchTurn();
-    }
-    switchTurn();
-    return mark.innerText;
-  }
-
-  function switchTurn() {
-    if (document.turn === "X") {
-      document.turn = "O";
-    } else {
-      document.turn = "X";
-    }
-    document.getElementById("message").innerHTML = document.turn + " gets to go";
-    winner();
-  }
-
-  // function getBox(number) {
-  //   return document.getElementById("space"+number).innerHTML;
-  // }
-
   //user interface logic:
-
-
   $("button#start").click(function() {
     startGame();
     $("#board").show();
     $("button#start").hide();
     $("button#reset").show();
   })
-  // $("button#reset").click(function() {
-  //   reset();
-  // })
+  $("button#reset").click(function() {
+     reset();
+   })
+
+   Square.prototype.ypos = function () {
+     return this.ynumber[this.yindex].innerText;
+   };
+
+
+
+
 
 });
+
+
+  // var squares= [];
+  // function createArray() {
+  //    squares.push(newSquare1,newSquare2,newSquare3,newSquare4,newSquare5,newSquare6,newSquare7,newSquare8,newSquare9);
+  //    return squares;
+  // }
+  // //
+  // // for(i = 0; i <= squares.length; i++){
+  // //   return squares[i].ypos();
+  // // }
 
 
 
